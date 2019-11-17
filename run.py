37e007719fbb75edf693f99aada518580e6689a8
app-generator <app-generator@appseed.us>
2019-11-17 12:09:20 +0200

    code refactoring

--- run.py
+++ run.py
@@ -1,27 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
-Light Bootstrap Dashboard - coded in Flask
-
-Author  : AppSeed App Generator
-Design  : Creative-Tim.com
-License : MIT 
-Support : https://appseed.us/support 
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
 """
 
-# used by the static export
-import click
-from   flask_frozen import Freezer
-
-from app import app
-from app import db
-
-# define custom command 
-@app.cli.command()
-def build():
-    freezer = Freezer(app)
-    freezer.freeze()
-
-if __name__ == "__main__":
-
-    db.create_all()
-    app.run() 
+from app import app, db
